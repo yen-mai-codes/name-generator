238c3a1f9842245230a7757c02e9818b0b2788b5
Yen <maiyn@ucmail.uc.edu>
2025-05-01 15:51:17 -0400

    Add FAQ page

---
+++ faq.py
@@ -0,0 +1,16 @@
+import streamlit as st
+st.markdown("# FAQ 🌐")
+
+st.markdown("### What is POS?")
+st.markdown("""
+From Wikipedia, 05/01/2024: \n
+            
+In corpus linguistics, part-of-speech tagging (POS tagging or PoS tagging or POST), also 
+called *grammatical tagging* is the process of marking up a word in a text (corpus) as 
+corresponding to a particular part of speech, based on both its definition and its context. 
+A simplified form of this is commonly taught to school-age children, in the identification 
+of words as nouns, verbs, adjectives, adverbs, etc.
+
+The complete POS list can be found on the Penn Treebank Project.
+""")
+
